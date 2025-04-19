6c3fedb0da950eb1e78d8cbb2aca2fec1470ea34
@y.filipov <y.filipov@surfcoffee.ru>
2025-04-19 12:27:47 +0300

    - добавлена отдельная кнопка для оплаты/займа - исправлено отображение имени в диалоге транзакции - небольшой рефакторинг изображений

--- app/src/main/java/com/example/debt/ui/screens/MainUserScreen.kt
+++ app/src/main/java/com/example/debt/ui/screens/MainUserScreen.kt
@@ -6,19 +6,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -32,7 +28,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,7 +41,6 @@ import org.koin.androidx.compose.koinViewModel
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainUserScreen() {
-    val context = LocalContext.current
     val viewModel: DebtorViewModel = koinViewModel()
 
     val debtors by viewModel.debtors.collectAsState(initial = emptyList())
@@ -57,7 +51,7 @@ fun MainUserScreen() {
 
     Box(
         modifier = Modifier
-            .padding(16.dp),
+            .padding(start = 16.dp, end = 16.dp, top = 16.dp,),
         contentAlignment = Alignment.Center
     ) {
         Column {
@@ -78,7 +72,7 @@ fun MainUserScreen() {
                 items(debtors.size) { debtor ->
                     DebtorCard(
                         debtor = debtors[debtor],
-                        onDebtorClick = {
+                        onDeleteDebtorClick = {
                             viewModel.deleteDebtor(debtors[debtor].id)
                         },
                         onPaymentClick = {
@@ -86,6 +80,9 @@ fun MainUserScreen() {
                             showPaymentDialog = true
                         }
                     )
+                }
+                item {
+                    Spacer(Modifier.size(24.dp))
                 }
             }
         }
