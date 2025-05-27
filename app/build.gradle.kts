1c6eb4e4072de824b634e4a15deaa5863eb4584d
hllwrld <hllwrld1006@gmail.com>
2025-05-27 17:37:54 +0300

    поднята версия приложения

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.debt"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "4.0"
+        versionCode = 5
+        versionName = "4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
